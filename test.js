277cd56a0af7030a13a3358cfec12ffe0e8b51aa
Miguel Afonso <miguel.afonso@digital.justice.gov.uk>
2020-12-07 14:51:02 +0000

    init npm

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var mappingTemplate = require('api-gateway-mapping-template')
 
 var vtl = fs.readFileSync(__dirname + '/mapping.vtl', 'utf8');
-var payload = fs.readFileSync(__dirname + '/sample2.json', 'utf8');
+var payload = fs.readFileSync(__dirname + '/sample.json', 'utf8');
 var apikey = 'test-a792e045f206a52ac0530ed70a1dfab8'
 var result = mappingTemplate({template: vtl, payload: payload, params: {header: {name: "x-api-key"}}})
 //console.dir(result);
