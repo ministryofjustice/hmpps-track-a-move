9c0cbe349bf079ef9689d2e1563163d003d3c195
Miguel Afonso <miguel.afonso@digital.justice.gov.uk>
2020-12-07 14:37:12 +0000

    Add CI

--- test.js
+++ test.js
@@ -1,13 +1,11 @@
 var fs = require('fs');
 var mappingTemplate = require('api-gateway-mapping-template')
 
-
 var vtl = fs.readFileSync(__dirname + '/mapping.vtl', 'utf8');
-console.log(vtl);
+var payload = fs.readFileSync(__dirname + '/sample2.json', 'utf8');
+var apikey = 'test-a792e045f206a52ac0530ed70a1dfab8'
+var result = mappingTemplate({template: vtl, payload: payload, params: {header: {name: "x-api-key"}}})
+//console.dir(result);
 
-var payload = fs.readFileSync(__dirname + '/sample.json', 'utf8');
-console.log(payload);
 
-var result = mappingTemplate({template: vtl, payload: payload})
-console.dir(result);
-
+process.stdout.write(result);
