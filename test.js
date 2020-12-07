c29514971462bf936268135cbe263bf7dbc66c3f
Miguel Afonso <miguel.afonso@digital.justice.gov.uk>
2020-12-07 16:57:21 +0000

    Run multiple tests

--- test.js
+++ test.js
@@ -4,8 +4,21 @@ var mappingTemplate = require('api-gateway-mapping-template')
 var vtl = fs.readFileSync(__dirname + '/mapping.vtl', 'utf8');
 var payload = fs.readFileSync(__dirname + '/sample.json', 'utf8');
 var apikey = 'test-a792e045f206a52ac0530ed70a1dfab8'
-var result = mappingTemplate({template: vtl, payload: payload, params: {header: {name: "x-api-key"}}})
 //console.dir(result);
 
 
-process.stdout.write(result);
+function validateTemplate() {
+    var payload = fs.readFileSync(__dirname + '/sample.json', 'utf8');
+    var result = mappingTemplate({template: vtl, payload: payload, params: {header: {name: "x-api-key"}}})
+    return result
+}
+
+function validateOptionalFieldSpeed() {
+    var payload = fs.readFileSync(__dirname + '/sample2.json', 'utf8');
+    var result = mappingTemplate({template: vtl, payload: payload, params: {header: {name: "x-api-key"}}})
+    return result
+}
+
+
+process.stdout.write(validateTemplate());
+process.stdout.write(validateOptionalFieldSpeed());
