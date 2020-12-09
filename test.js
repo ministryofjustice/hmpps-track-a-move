6870acff06a1ac4167f67d389b3cc38f8e70ae63
Miguel Afonso <miguel.afonso@digital.justice.gov.uk>
2020-12-09 10:41:56 +0000

    specific tests

--- test.js
+++ test.js
@@ -1,24 +1,34 @@
 var fs = require('fs');
 var mappingTemplate = require('api-gateway-mapping-template')
 
-var vtl = fs.readFileSync(__dirname + '/mapping.vtl', 'utf8');
-var payload = fs.readFileSync(__dirname + '/sample.json', 'utf8');
-var apikey = 'test-a792e045f206a52ac0530ed70a1dfab8'
 //console.dir(result);
 
 
-function validateTemplate() {
-    var payload = fs.readFileSync(__dirname + '/sample.json', 'utf8');
-    var result = mappingTemplate({template: vtl, payload: payload, params: {header: {name: "x-api-key"}}})
+function runTemplate(payload) {
+    const vtl = fs.readFileSync(__dirname + '/mapping.vtl', 'utf8');
+    const apikey = 'test-a792e045f206a52ac0530ed70a1dfab8';
+
+    const result = mappingTemplate({template: vtl, payload: payload, params: {header: {"x-api-key": apikey}}});
     return result
 }
 
-function validateOptionalFieldSpeed() {
+function testRunTemplate() {
     var payload = fs.readFileSync(__dirname + '/sample2.json', 'utf8');
-    var result = mappingTemplate({template: vtl, payload: payload, params: {header: {name: "x-api-key"}}})
-    return result
+    var result = runTemplate(payload)
+    console.log(result);
+
+    jsObj = JSON.parse(result);
+    console.log(jsObj['DeliveryStreamName'] == true)
+
+
+    //TODO: Use assertions
+    if ("DeliveryStreamName" in jsObj) {
+        console.log("DeliveryStreamName exists");
+    }
+    else
+    {
+        console.error('Failed.')
+    }
 }
 
-
-process.stdout.write(validateTemplate());
-process.stdout.write(validateOptionalFieldSpeed());
+testRunTemplate();
