33f37dc458f9555e0c54ed9d0b009ee203a1efe7
Miguel Afonso <miguel.afonso@digital.justice.gov.uk>
2020-12-17 10:54:40 +0000

    Athena create queries

---
+++ create_table_serco.sql
@@ -0,0 +1,27 @@
+CREATE EXTERNAL TABLE `track_a_move_dev.serco`(
+  `altitude` float COMMENT 'from deserializer', 
+  `bearing` float COMMENT 'from deserializer', 
+  `journey_id` string COMMENT 'from deserializer', 
+  `latitude` float COMMENT 'from deserializer', 
+  `longitude` float COMMENT 'from deserializer', 
+  `precision_hdop` float COMMENT 'from deserializer', 
+  `precision_vdop` float COMMENT 'from deserializer', 
+  `received_timestamp` string COMMENT 'from deserializer', 
+  `speed` float COMMENT 'from deserializer', 
+  `tracking_id` string COMMENT 'from deserializer', 
+  `tracking_timestamp` string COMMENT 'from deserializer', 
+  `vehicle_registration` string COMMENT 'from deserializer', 
+  `vehicle_vin` string COMMENT 'from deserializer')
+PARTITIONED BY ( 
+  `year` int, 
+  `month` int, 
+  `day` int, 
+  `hour` int)
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.IgnoreKeyTextOutputFormat'
+LOCATION
+  's3://cloud-platform-87b76c842f5b97891eaf7e83a7818491/serco'
